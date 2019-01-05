9964fe994e6554abf2f8c75966c2d0441e82a325
root <root@ip-172-31-46-221.ap-northeast-1.compute.internal>
2019-01-05 08:58:51 +0000

    endpoint change

--- js/clip.js
+++ js/clip.js
@@ -1,5 +1,5 @@
 var constants = Object.freeze({
-    "ES_BASE_URL": "//clip.mokuba.tech:9200/clip/clip/_search",
+    "ES_BASE_URL": "//clip.mokuba.tec/clip/clip/_search",
     "PER_PAGE_COUNT": 20
 });
 
